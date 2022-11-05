c525636ae9b23432f41671133490dc40da3891d3
enesmalikdmn <m.duman@digitastic.app>
2022-11-05 16:59:22 +0300

    fix: url changed

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -28,7 +28,7 @@ const Layout = ({ children }) => {
                     name="twitter:card" 
                     content="Petkar Ile De France Damızlık Çiftliği"
                 ></meta>
-                <link rel="canonical" href="https://xn--petkarildefranceiftlii-f7b36k.com"></link>
+                <link rel="canonical" href="https://petkariledefranceciftligi.com"></link>
             </Head>
 
             {children}
